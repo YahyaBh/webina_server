e83aa741482fe9b378a9ee46fa5d7fd36ecb0cde
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-02-07 09:43:25 +0100

    blogs start

--- database/migrations/2023_02_06_180211_create_blogs_table.php
+++ database/migrations/2023_02_06_180211_create_blogs_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->string('image');
             $table->timestamps();
         });
     }
