6b943252a636129a7e4713a0da999b1f3decd908
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-03-15 23:24:59 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone')->unique()->nullable();
             $table->string('google_id')->unique()->nullable();
             $table->string('avatar')->default('user_default.png');
             $table->timestamp('email_verified_at')->nullable();
