ba880a6a49c4e6633ea4e1bc4e23ee359169f5c8
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-03-21 22:32:42 -0700

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,30 +9,23 @@ class ContactController extends Controller
 {
     public function store_message(Request $request)
     {
-        try {
-            $request->validate([
-                'email' => 'required|email',
-                'name' => 'required|min:8',
-                'message' => 'required|max:400',
-            ]);
+        $request->validate([
+            'email' => 'required|email',
+            'name' => 'required|min:8',
+            'message' => 'required|max:400',
+        ]);
 
 
-            Contact::create([
-                'email' => $request->email,
-                'name' => $request->name,
-                'message' => $request->message,
-            ]);
+        Contact::create([
+            'email' => $request->email,
+            'name' => $request->name,
+            'message' => $request->message,
+        ]);
 
 
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Message sent successfully',
-            ], 200);
-        } catch (\Exception $e) {
-            return response()->json([
-                'status' => 'failed',
-                'message' => 'Failed With : ' . $e->getMessage(),
-            ], 401);
-        }
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Message sent successfully',
+        ], 200);
     }
 }
